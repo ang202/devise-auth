62f0cc2f71fb8067d3dfa5fad476c6c940a464df
Tony <angkaithong2018@gmail.com>
2021-01-19 21:20:43 +0800

    Config csrf

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::SessionsController < Devise::SessionsController
     def create
       if @user.valid_password?(sign_in_params[:password])
         sign_in @user
-        json_message('User signed in succesfully',true,@user,:ok)
+        # json_message('User signed in succesfully',true,@user,:ok)
         payload = { user_id: @user.id }
         session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
         tokens = session.login
@@ -15,16 +15,16 @@ class Api::V1::SessionsController < Devise::SessionsController
                           value: tokens[:access],
                           httponly: true,
                           secure: Rails.env.production?)
-        # render json: { csrf: tokens[:csrf] } 
+        render json: { csrf: tokens[:csrf] } 
       else
         json_message "Email or password invalid", false, {},:bad_request
       end
     end
   
     def destroy
-      sign_out @user
       session = JWTSessions::Session.new(payload: payload)
       session.flush_by_access_payload
+      json_message "Signed out successfully", true, {},:ok
     end
     
     private
@@ -38,6 +38,5 @@ class Api::V1::SessionsController < Devise::SessionsController
     end
   
     def respond_to_on_destroy
-      head :ok
     end
 end
